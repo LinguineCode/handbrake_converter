ed883e995f14b2d337cbe8db366c0b219db71c43
sauce@freenode <sean@notamused.org>
2014-11-15 14:35:32 -0500

    Update handbrake-converter.sh

--- handbrake-converter.sh
+++ handbrake-converter.sh
@@ -1,16 +1,29 @@
-#!/bin/bash
+#!/bin/bash -e
 
-SRC="$HOME/Downloads/unrared/"
-DST="$HOME/Downloads/iPhone-movies/"
+if [ -z "$2" ]; then
+ echo "Usage: $0 <source path for recursive movie search> <output path>"
+ exit 1
+elif [ ! -d "$1" ]; then
+ echo "Source path does not exist: $1"
+ exit 1
+elif [ ! -d "$2" ]; then
+ echo "Destination path does not exist: $2"
+ exit 1
+fi
+
+SRC="$1"
+DST="$2"
 EXT="mp4"
-PRESET="iPhone & iPod Touch"
-RM_SRC="true"
+PRESET="iPad"
+#RM_SRC="true"
 
-find $SRC -type f | while read line
+find -E "$SRC" -type f -regex '.*\.(mkv|mp4)' | grep -v \/Sample\/ | while read line
 do
- outfile="$(basename "$line")-iPhone.$EXT"
+ outfile="$(basename "$line")-$PRESET.$EXT"
  if [ ! -e "$DST/$outfile" ]; then
   echo ""  | HandBrakeCLI --preset="$PRESET" -i "$line" -o "$DST/$outfile"
-  if [ "$RM_SRC" ]; then trash "$line" ;fi
+  if [ "$RM_SRC" ]; then
+   rm -rf "$line"
+  fi
  fi
 done
